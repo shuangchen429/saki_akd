fbabe58e3fbf6aeb73cf816e8ba097d2e98cdff2
shuangchen429 <2393445889@qq.com>
2025-03-19 01:45:47 +0800

    Update saki_aki_predictory.py

--- saki_aki_predictory.py
+++ saki_aki_predictory.py
@@ -82,20 +82,3 @@ if st.button("Predict"):
     ax.axis('off')
     plt.savefig("prediction_text.png", bbox_inches='tight', dpi=300)
     st.image("prediction_text.png")
-
-
-    # 计算 SHAP 值
-    explainer = shap.LinearExplainer(model)
-    shap_values = explainer.shap_values(pd.DataFrame([feature_values], columns=feature_ranges.keys()))
-
-    # 生成 SHAP 力图
-    class_index = predicted_class  # 当前预测类别
-    shap_fig = shap.force_plot(
-        explainer.expected_value[class_index],
-        shap_values[:,:,class_index],
-        pd.DataFrame([feature_values], columns=feature_ranges.keys()),
-        matplotlib=True,
-    )
-    # 保存并显示 SHAP 图
-    plt.savefig("shap_force_plot.png", bbox_inches='tight', dpi=1200)
-    st.image("shap_force_plot.png")
