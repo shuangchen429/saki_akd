2b9ebdcdeeeb6474533c7e6c1a13ccf9c4f8f023
shuangchen429 <2393445889@qq.com>
2025-05-17 22:13:38 +0800

    saki_aki_predictory1.py

--- saki_aki_predictory.py
+++ saki_aki_predictory.py
@@ -19,19 +19,19 @@ except Exception as e:
 # 定义特征参数
 feature_ranges = {
     'ACEI/ARB': {"type": "categorical", "options": [0, 1]},
-    'APS III': {"type": "numerical", "min": 0, "max": 215, "default": 0, "unit": "points"},
+    'APS III': {"type": "numerical", "min": 0, "max": 215, "default": , "unit": "points"},
     'CRRT': {"type": "categorical", "options": [0, 1]},
     'Cerebrovascular Disease': {"type": "categorical", "options": [0, 1]},
-    'LODS': {"type": "numerical", "min": 0, "max": 22, "default": 0, "unit": "points"},
-    'Los_inf._AB': {"type": "numerical", "min": 0, "max": 7, "default": 0, "unit": "hours"},
-    'MBP': {"type": "numerical", "min": 0, "max": 200, "default": 60, "unit": "mmHg"},
+    'LODS': {"type": "numerical", "min": 0, "max": 22, "default": , "unit": "points"},
+    'Los_inf._AB': {"type": "numerical", "min": 0,  "default": , "unit": "hours"},
+    'MBP': {"type": "numerical", "min": 0,  "default": , "unit": "mmHg"},
     'Mechanical Ventilation': {"type": "categorical", "options": [0, 1]},
     'Paraplegia': {"type": "categorical", "options": [0, 1]},
-    'Resp Rate': {"type": "numerical", "min": 0, "max": 50, "default": 18, "unit": "breaths/min"},
-    'Scr Baseline': {"type": "numerical", "min": 0, "max": 5000, "default": 60, "unit": "mmol/L"},
-    'SpO2': {"type": "numerical", "min": 0, "max": 100, "default": 100, "unit": "%"},
+    'Resp Rate': {"type": "numerical", "min": 0, "default": , "unit": "breaths/min"},
+    'Scr Baseline': {"type": "numerical", "min": 0,  "default": , "unit": "mmol/L"},
+    'SpO2': {"type": "numerical", "min": 0, "max": 100, "default": , "unit": "%"},
     'Vasoactive Agent': {"type": "categorical", "options": [0, 1]},
-    'Weight': {"type": "numerical", "min": 30, "max": 200, "default": 60, "unit": "kg"}
+    'Weight': {"type": "numerical", "min": 0,  "default": , "unit": "kg"}
 }
 
 # 页面布局
